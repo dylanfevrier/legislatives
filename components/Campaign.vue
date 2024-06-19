6044841867ee47d7da5bdf3cb36005e6d60248ed
Dylan Fevrier <hello@dylanfevrier.com>
2024-06-18 21:10:56 -0400

    fix: button

--- components/Campaign.vue
+++ components/Campaign.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="bg-yellow">
-  <div class="px-8 w-full lg:w-[1200px] mx-auto py-10 flex flex-col items-center gap-y-4">
+  <div class="px-4 w-full lg:w-[1200px] mx-auto py-10 flex flex-col items-center gap-y-4">
     <h2 class="text-center" >Prenez part à la campagne</h2>
 
     <a href="https://forms.gle/hYXh5hZWSpH9f5Ct5" target="_blank" class="btn btn-black">Je participe</a>
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <div>
     <Header  />
 
-    <Hero class="px-8 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
+    <Hero class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
 
-    <Statement class="px-8 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
+    <Statement class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
 
     <Campaign class="my-10 lg:my-15"/>
 
-    <div class="px-8 w-full lg:w-[1200px] mx-auto flex flex-col lg:flex-row gap-x-6 gap-y-4 my-5 lg:my-15">
+    <div class="px-4 w-full lg:w-[1200px] mx-auto flex flex-col lg:flex-row gap-x-6 gap-y-4 my-5 lg:my-15">
       <Applicant class="w-full lg:w-1/2" />
       <Alternate class="w-full lg:w-1/2"/>
     </div>
