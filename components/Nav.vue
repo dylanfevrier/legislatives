ee597068bef8b852dc97733f09a57c30b4a923eb
Dylan Fevrier <hello@dylanfevrier.com>
2024-06-21 12:35:56 -0400

    add nav

---
+++ components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="flex flex-col lg:flex-row justify-end gap-x-4">
+    <a class="hover:underline font-medium" href="#statement">Profession de foi</a>
+    <a href="#applicant" class="hover:underline font-medium">Candidat</a>
+    <a href="#events" class="hover:underline font-medium">Prochains événements</a>
+  </nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,22 @@
   <div>
     <Header  />
 
+    <Nav class="px-4 w-full lg:w-[1200px] mx-auto mt-2 mb-10 lg:mb-15" />
+
     <Hero class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
 
-    <Statement class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
+    <Statement id="statement" class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
 
     <Campaign class="my-10 lg:my-15"/>
 
-    <div class="px-4 w-full lg:w-[1200px] mx-auto flex flex-col lg:flex-row gap-x-6 gap-y-4 my-5 lg:my-15">
+    <div id="applicant" class="px-4 w-full lg:w-[1200px] mx-auto flex flex-col lg:flex-row gap-x-6 gap-y-4 my-5 lg:my-15">
       <Applicant class="w-full lg:w-1/2" />
       <Alternate class="w-full lg:w-1/2"/>
     </div>
 
     <Press class="my-10 lg:my-15"/>
 
-    <Events class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
+    <Events id="events" class="px-4 w-full lg:w-[1200px] mx-auto my-10 lg:my-15" />
 
     <Footer />
   </div>
