c6375671001f78be66ba7522f1cab79b50998bee
Dylan Fevrier <hello@dylanfevrier.com>
2024-06-21 11:56:22 -0400

    add green button

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,7 +5,7 @@
         <h1 class="mb-2">Le 29 juin et le 6 juillet, je vote Nouveau Front Populaire</h1>
         <p>1<sup>ère</sup> circonscription • Canada et États-Unis</p>
       </div>
-      <a href="https://nfp-programme.s3.ca-central-1.amazonaws.com/programme-nfp.pdf" target="_blank" class="btn btn-red">Télécharger le programme</a>
+      <a href="https://nfp-programme.s3.ca-central-1.amazonaws.com/programme-nfp.pdf" target="_blank" class="btn btn-green">Télécharger le programme</a>
     </div>
 
     <div class="flex lg:justify-end w-full lg:w-1/3">
